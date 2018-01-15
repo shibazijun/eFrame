01286ee51729051f2a946cad2d04880cf098afe3
shibazijun <ajun@ec1001.com>
2018-01-15 18:05:13 +0800

    reset

--- public/js/public.js
+++ public/js/public.js
@@ -1,6 +1,9 @@
 var e={
 	drag:function(id){
-		var obj =document.getElementById(id)
+		var obj =document.getElementById(id);
+		var dis={}
+			dis.X=0;
+			dis.Y=0;
 		obj.onmousedown=function(){
 		}
 	}
